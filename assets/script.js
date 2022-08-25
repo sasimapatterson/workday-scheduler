97258692f15f31c9dec5551dfa7430e7a1c7cc7a
Sasi Patterson <sweetpea_th@hotmail.com>
2022-08-25 19:10:27 -0400

    working on color coded with moment.js

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,45 @@
-var today = document.querySelector('#currentDay');
-// var scheculeForm = document.querySelector('.scheduler');
-// var textArea = document.querySelector('.note');
-// var saveIcon = document.querySelector('.save-icon');
+var today = $('#currentDay');
+var scheculeForm = $('.scheduler');
+var textArea = $('.note');
+var saveIcon = $('.save-icon');
 
-today.textContent = moment();
+today.textContent = moment().format('dddd, MMMM, Do YYYY, hh:mm:ss a');
 
 // text input area
-$('.scheduler').on('click', '.note', function () {
-    var parentId = $(this).parent().attr('data-id');
-    var textArea = $(this).prev().val();
-    console.log(textArea);
-    localStorage.setItem(parentId, textArea);
-});
+// $('.container').on('click', '.note', function () {
+//     var parentId = $(this).parent().attr('data-id');
+//     var textArea = $(this).prev().val();
+//     console.log(textArea);
+//     localStorage.setItem(parentId, textArea);
+// });
 
-$('.scheduler').each(function () {
-    var parentId = $(this).parent().attr('data-id');
-    var textArea = localStorage.getItem(parentId);
-    $(this).val(textArea);
-});
+// $('.note').each(function () {
+//     var parentId = $(this).parent().attr('data-id');
+//     var textArea = localStorage.getItem(parentId);
+//     $(this).val(textArea);
+// });
+
+
+var currentTime = moment().hour();
+for (var i = 9; i < 20; i++) {
+    if(i == currentTime){
+        $("textarea").css({"background-color": "#ff6961", color: "white"});
+    } else if (i < currentTime) {
+        $("textarea").css({"background-color": "#d3d3d3", color: "white"});
+    } else {
+        $("textarea").css({"background-color": "#77dd77", color: "white"});
+    }
+}  
+    
+    // var currentTime = moment().format('HH');
+    // console.log(currentTime);
+    
+
+    // for (var i = 9; i < 20; i++){
+    //     if(i == currentTime)$('.note$[i]').addClass('present');
+    //     if(i < currentTime)$('.note$[i]').addClass('past');
+    //     if(i > currentTime)$('.note$[i]').addClass('future');
+    // }
+ 
+ 
+    
