12a7ecce662ae4b23d4700710ab54ee755289d99
Sasi Patterson <sweetpea_th@hotmail.com>
2022-08-25 16:33:20 -0400

    trying to get local storage to work.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,20 @@
 var today = document.querySelector('#currentDay');
+// var scheculeForm = document.querySelector('.scheduler');
+// var textArea = document.querySelector('.note');
+// var saveIcon = document.querySelector('.save-icon');
 
 today.textContent = moment();
+
+// text input area
+$('.scheduler').on('click', '.note', function () {
+    var parentId = $(this).parent().attr('data-id');
+    var textArea = $(this).prev().val();
+    console.log(textArea);
+    localStorage.setItem(parentId, textArea);
+});
+
+$('.scheduler').each(function () {
+    var parentId = $(this).parent().attr('data-id');
+    var textArea = localStorage.getItem(parentId);
+    $(this).val(textArea);
+});
