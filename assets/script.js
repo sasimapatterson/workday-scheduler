5ebea8c137d47b6a1c04d1a64847ab6737de29ac
Sasi Patterson <sweetpea_th@hotmail.com>
2022-08-25 21:41:17 -0400

    get color coded to work according to current time

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,30 @@
-var today = $('#currentDay');
+var today = document.querySelector('#currentDay');
 var scheculeForm = $('.scheduler');
 var textArea = $('.note');
 var saveIcon = $('.save-icon');
 
 today.textContent = moment().format('dddd, MMMM, Do YYYY, hh:mm:ss a');
 
-// text input area
-// $('.container').on('click', '.note', function () {
-//     var parentId = $(this).parent().attr('data-id');
-//     var textArea = $(this).prev().val();
-//     console.log(textArea);
-//     localStorage.setItem(parentId, textArea);
-// });
-
-// $('.note').each(function () {
-//     var parentId = $(this).parent().attr('data-id');
-//     var textArea = localStorage.getItem(parentId);
-//     $(this).val(textArea);
-// });
-
+// text in
+$('.schedule').on('click', '.save-icon', function () {
+    var parentId = $(this).parent().attr('data-id');
+    console.log(parentId);
+    var textArea = $(this).prev().val();
+    console.log(textArea);
+    localStorage.setItem(parentId, textArea);
+});
 
 var currentTime = moment().hour();
-for (var i = 9; i < 20; i++) {
-    if(i == currentTime){
-        $("textarea").css({"background-color": "#ff6961", color: "white"});
-    } else if (i < currentTime) {
-        $("textarea").css({"background-color": "#d3d3d3", color: "white"});
-    } else {
-        $("textarea").css({"background-color": "#77dd77", color: "white"});
-    }
-}  
-    
-    // var currentTime = moment().format('HH');
-    // console.log(currentTime);
-    
 
-    // for (var i = 9; i < 20; i++){
-    //     if(i == currentTime)$('.note$[i]').addClass('present');
-    //     if(i < currentTime)$('.note$[i]').addClass('past');
-    //     if(i > currentTime)$('.note$[i]').addClass('future');
-    // }
- 
- 
-    
+
+$('.note').each(function () {
+    var parentId = $(this).parent().attr('data-id');
+    var textArea = localStorage.getItem(parentId);
+    $(this).val(textArea);
+
+    if(parentId < currentTime){$(this).css("background-color", '#d3d3d3');}
+    if(parentId == currentTime){$(this).css("background-color", '#ff6961');}
+    if(parentId > currentTime){$(this).css("background-color", '#77dd77');}
+});
+
+
